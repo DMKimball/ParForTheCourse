7daad553544764f88b1b7c53451f791e659c5e71
Gary Chew <garylchew@gmail.com>
2017-10-21 02:58:45 -0700

    Ball Test Scene

---
+++ Assets/Scripts/Interactions_GolfBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactions_GolfBall : MonoBehaviour {
+
+    public GameManager gm;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Goal"))
+        {
+            gm.victory();
+        }
+    }
+}
